bdfc5870a7667996ffae62f53914756fb2aeb054
Vencator <rys@gmx.us>
2023-05-25 18:28:10 +0200

    add svgs, first steps

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,11 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Ubuntu } from 'next/font/google'
+import Header from './components/header/header'
 
-const inter = Inter({ subsets: ['latin'] })
+const ubuntu = Ubuntu({ 
+  subsets: ['latin'],
+  weight: ["300", "400", "500", "700"]
+})
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +15,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={ubuntu.className}>
+        <Header />
+        {children}
+        </body>
     </html>
   )
 }
